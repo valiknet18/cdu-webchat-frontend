4b0dd10f4997156f6f7d1e43f1f7f48d8e79d0f9
Valentyn Hrynevych <valik.v1per@gmail.com>
2017-06-17 15:46:30 +0300

    Implemented add images

--- src/app/chat/messages/message.component.ts
+++ src/app/chat/messages/message.component.ts
@@ -3,7 +3,14 @@ import {Message} from "../../shared/models/message";
 
 @Component({
   selector: 'message',
-  templateUrl: './message.component.html'
+  templateUrl: './message.component.html',
+  styles: [`
+      .message-image {
+        max-width: 250px;
+        display: inline-block;
+        margin: 2px 10px;
+      }
+  `]
 })
 export class MessageComponent {
   @Input() message: Message;
